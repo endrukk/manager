7b7d11a74502d29b7682a290dfc724a1e7ac91ac
endrukk <endrukk@htomail.com>
2018-08-29 22:32:28 +0200

    WP like menus Finnally ok

--- resources/views/admin/menus/edit.blade.php
+++ resources/views/admin/menus/edit.blade.php
@@ -1,120 +1,264 @@
 @extends('layouts.default')
 
 @section('content')
-    <style>
-        body {
+
+
+    <style type="text/css">
+        /**
+         * Nestable
+         */
+        .dd {
+            position: relative;
+            display: block;
             margin: 0;
             padding: 0;
-            font-family: sans-serif;
+            max-width: 600px;
+            list-style: none;
+            font-size: 13px;
+            line-height: 20px;
         }
 
-        .drag {
-            display: flex;
-
+        .dd-list {
+            display: block;
+            position: relative;
+            margin: 0;
+            padding: 0;
+            list-style: none;
         }
 
-        #menu_manager > .drag-drop-area,
-        #menu_final .drag-drop-area{
-            border: solid 1px;
-            width: 200px;
-            min-height: 35px;
-            margin: 15px;
-            list-style-type: none;
+        .dd-list .dd-list {
+            padding-left: 30px;
         }
-        #menu_item_drag .drag-drop-area  .draggable-item > .move{
+
+        .dd-collapsed .dd-list {
             display: none;
         }
-        /*#menu_item_drag > .drag-drop-area > .draggable-item > .move,*/
-        /*#menu_item_drag > .drag-drop-area > .drag-drop-area{*/
-            /*display: none;*/
-        /*}*/
-        /*#menu_final > .drag-drop-area > .draggable-item > .move,*/
-        /*#menu_final > .drag-drop-area > .drag-drop-area{*/
-            /*display: block;*/
-        /*}*/
 
-        .noselect {
-            -webkit-touch-callout: none; /* iOS Safari */
-            -webkit-user-select: none; /* Safari */
-            -khtml-user-select: none; /* Konqueror HTML */
-            -moz-user-select: none; /* Firefox */
-            -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome and Opera */
+        .dd-item,
+        .dd-empty,
+        .dd-placeholder {
+            display: block;
+            position: relative;
+            margin: 0;
+            padding: 0;
+            min-height: 20px;
+            font-size: 13px;
+            line-height: 20px;
         }
 
+        .dd-handle {
+            display: block;
+            height: 30px;
+            margin: 5px 0;
+            padding: 5px 10px;
+            color: #333;
+            text-decoration: none;
+            font-weight: bold;
+        }
+
+
+        .dd-item > button {
+            display: block;
+            position: relative;
+            cursor: pointer;
+            float: left;
+            width: 25px;
+            height: 20px;
+            margin: 5px 0;
+            padding: 0;
+            text-indent: 100%;
+            white-space: nowrap;
+            overflow: hidden;
+            border: 0;
+            background: transparent;
+            font-size: 12px;
+            line-height: 1;
+            text-align: center;
+            font-weight: bold;
+        }
+
+        .dd-item > button:before {
+            content: '+';
+            display: block;
+            position: absolute;
+            width: 100%;
+            text-align: center;
+            text-indent: 0;
+        }
+
+        .dd-item > button[data-action="collapse"]:before {
+            content: '-';
+        }
+
+        .dd-placeholder,
+        .dd-empty {
+            margin: 5px 0;
+            padding: 0;
+            min-height: 30px;
+            background: #f2fbff;
+            border: 1px dashed #b6bcbf;
+            box-sizing: border-box;
+            -moz-box-sizing: border-box;
+        }
+
+        .dd-empty {
+            border: 1px dashed #bbb;
+            min-height: 100px;
+            background-color: #e5e5e5;
+            background-image: -webkit-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff),
+            -webkit-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff);
+            background-image: -moz-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff),
+            -moz-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff);
+            background-image: linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff),
+            linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff);
+            background-size: 60px 60px;
+            background-position: 0 0, 30px 30px;
+        }
+
+        .dd-dragel {
+            position: absolute;
+            pointer-events: none;
+            z-index: 9999;
+        }
+
+        .dd-dragel > .dd-item .dd-handle {
+            margin-top: 0;
+        }
+
+        .dd-dragel .dd-handle {
+            -webkit-box-shadow: 2px 4px 6px 0 rgba(0, 0, 0, .1);
+            box-shadow: 2px 4px 6px 0 rgba(0, 0, 0, .1);
+        }
+
+        /**
+         * Nestable Extras
+         */
+        .nestable-lists {
+            display: block;
+            clear: both;
+            padding: 30px 0;
+            width: 100%;
+            border: 0;
+            border-top: 2px solid #ddd;
+            border-bottom: 2px solid #ddd;
+        }
+
+
+
+        @media only screen and (min-width: 700px) {
+            .dd {
+                float: left;
+                width: 48%;
+            }
+
+            .dd + .dd {
+                margin-left: 2%;
+            }
+        }
+
+        .dd-hover > .dd-handle {
+            background: #2ea8e5 !important;
+        }
+
+        /**
+         * Nestable Draggable Handles
+         */
+
+        .dd-dragel > .dd3-item > .dd3-content {
+            margin: 0;
+        }
+
+        .dd3-item > button {
+            margin-left: 30px;
+        }
+
+        .dd3-handle {
+            position: absolute;
+            margin: 0;
+            left: 0;
+            top: 0;
+            cursor: pointer;
+            width: 30px;
+            text-indent: 100%;
+            white-space: nowrap;
+            overflow: hidden;
+            border: 1px solid #aaa;
+            background: #ddd;
+            background: -webkit-linear-gradient(top, #ddd 0%, #bbb 100%);
+            background: -moz-linear-gradient(top, #ddd 0%, #bbb 100%);
+            background: linear-gradient(top, #ddd 0%, #bbb 100%);
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+        }
+
+        .dd3-handle:before {
+            content: '≡';
+            display: block;
+            position: absolute;
+            left: 0;
+            top: 3px;
+            width: 100%;
+            text-align: center;
+            text-indent: 0;
+            color: #fff;
+            font-size: 20px;
+            font-weight: normal;
+        }
+
+        .dd3-handle:hover {
+            background: #ddd;
+        }
+
+        /**
+         * Socialite
+         */
+        .socialite {
+            display: block;
+            float: left;
+            height: 35px;
+        }
     </style>
 
-
     <div class="column is-11-fullhd is-offset-1-fullhd is-12-desktop is-gapless">
-        <div class="drag columns" id="menu_manager">
-            <div class="column is-4 drag-drop-area tags" id="menu_item_drag">
-                @foreach($menuItems as $item)
-                    <div class="tag is-primary is-large">
-                        <span class="move">
-                            <i class='fas fa-arrows-alt'></i>
-                        </span>
+        <div class="drag columns">
 
-                        <div class="drag-drop-area">
-                            <div>
-                                <span class="draggable-item" id="draggable_{{$item->id}}">
-                                    <span class="move">
-                                        <i class='fas fa-arrows-alt'></i>
-                                    </span>
-                                    {{$item->name}}
-                                    <span class="edit">
-                                        <i class='fas fa-edit'></i>
-                                    </span>
-                                    <div class="drag-drop-area item_{{$item->id}}"></div>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
+            <div class="column is-4">
+                <div class="dd" id="nestable">
+                    <ol class="dd-list">
+                        @foreach($menuItems as $item)
+                            <li class="dd-item" data-id="{{$item->id}}">
+                                <div class="dd-handle is-block tag is-primary is-large">Item {{$item->id}}</div>
+                            </li>
+                        @endforeach
+                    </ol>
+                </div>
             </div>
 
-            <div class="drag-drop-area" id="menu_final">
+            <div class="column is-8">
                 @if(isset($menuCurrent) && is_array($menuCurrent))
-                    @foreach($menuCurrent as $item)
-                        <div class="tag is-primary is-large">
-                            <span class="move">
-                                <i class='fas fa-arrows-alt'></i>
-                            </span>
-                            <div class="drag-drop-area">
-                                <div>
-                                    <span class="draggable-item" id="draggable_{{$item->id}}">
-                                        <span class="move">
-                                            <i class='fas fa-arrows-alt'></i>
-                                        </span>
-                                        {{$item->name}}
 
-                                        <span class="edit">
-                                            <i class='fas fa-edit'></i>
-                                        </span>
-                                    </span>
-                                </div>
-                                <div class="drag-drop-area">
+                    <div class="dd" id="nestable2">
+                        <ol class="dd-list">
+                            @foreach($menuCurrent as $item)
+                                <li class="dd-item" data-id="{{$item->id}}">
+                                    <div class="dd-handle tag is-block is-primary is-large">Item {{$item->id}}</div>
                                     @if (isset($item->children) && is_array($item->children))
-                                        @foreach($item->children as $child)
-                                            <div>
-                                                <span class="draggable-item" id="draggable_{{$item->id}}">
-                                                    <span class="move">
-                                                        <i class='fas fa-arrows-alt'></i>
-                                                    </span>
-                                                    {{$item->name}}
-                                                    <span class="edit">
-                                                        <i class='fas fa-edit'></i>
-                                                    </span>
-                                                </span>
-                                            </div>
-                                        @endforeach
+                                        <ol class="dd-list children">
+                                            @foreach($item->children as $child)
+                                                <li class="dd-item" data-id="{{$child->id}}" data-parent="{{$item->id}}">
+                                                    <div class="dd-handle is-block tag is-primary">Item {{$child->id}}</div>
+                                                </li>
+                                            @endforeach
+                                        </ol>
                                     @endif
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
+                                </li>
+                            @endforeach
+                        </ol>
+                    </div>
+
                 @endif
             </div>
+
         </div>
     </div>
 
